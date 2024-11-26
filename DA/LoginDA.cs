44bcd76008a1105d722abab51c9d847056a014f9
senna.devolder <senna.devolder@sintjozefbrugge.be>
2024-11-26 13:49:34 +0100

    Add project files.

---
+++ DA/LoginDA.cs
@@ -0,0 +1,93 @@
+﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+//we gebruiken hetgene die in deze mappen zit (klassen Database en Login)
+using prjloginmetlagenoefenenenenenen.Helper;
+using prjloginmetlagenoefenenenenenen.Model;
+
+namespace prjloginmetlagenoefenenenenenen.DA
+{
+    //alles programmeren van onze klasse login (we gebruiken daarvoor onze klasse Database, onze klasse Login
+    public class LoginDA
+    {
+
+        //we bekijken of de logingegevens correct zijn
+        //boolean om te controleren of ze correct zijn --> True // anders Fale
+        public static void LoginValidation(Login L)
+        {
+            //sqlconnection aanmaken --> verbinding met de databank te maken
+            MySqlConnection conn = Databank.MakeConnection();
+
+            string query = "SELECT COUNT(1) FROM login.tbllogin WHERE Username=@Username AND Password=@Password";
+            //commando maken --> zorgt ervoor dat de sql statement wordt ingezet
+            MySqlCommand sqlcmd = new MySqlCommand(query, conn);
+            //welk soort commando is dat?
+            sqlcmd.Parameters.AddWithValue("@Username", L.Username);
+            sqlcmd.Parameters.AddWithValue("@Password", L.Password);
+
+            //tellen van het aantal gevonden gegevens
+            int count = Convert.ToInt32(sqlcmd.ExecuteScalar());
+
+            if (count == 1)
+            {
+                MessageBox.Show("GElUKT!");
+            }
+            else
+            {
+                MessageBox.Show("niet gelukt!");
+            }
+        }
+
+        public static void AddUser(Login L)
+        {
+            MySqlConnection conn = Databank.MakeConnection();
+
+            string query = "INSERT INTO login.tbllogin(Username, Password) VALUES (@Username, @Password)";
+            MySqlCommand sqlcmd = new MySqlCommand(query, conn);
+            sqlcmd.Parameters.AddWithValue("@Username", L.Username);
+            sqlcmd.Parameters.AddWithValue("@Password", L.Password);
+
+            sqlcmd.ExecuteScalar();
+
+
+            MessageBox.Show("GElUKT!");
+        }
+
+        public static void ChangePass(Login L, string NewPass)
+        {
+            MySqlConnection conn = Databank.MakeConnection();
+
+            string query = "UPDATE login.tbllogin SET Password=@NewPass WHERE Username=@Username AND Password=@Password";
+            MySqlCommand sqlcmd = new MySqlCommand(query, conn);
+            sqlcmd.Parameters.AddWithValue("@Username", L.Username);
+            sqlcmd.Parameters.AddWithValue("@Password", L.Password);
+            sqlcmd.Parameters.AddWithValue("@NewPass", NewPass);
+
+            sqlcmd.ExecuteScalar();
+
+
+            MessageBox.Show("GElUKT!");
+        }
+
+        public static void Delete(Login L)
+        {
+            MySqlConnection conn = Databank.MakeConnection();
+
+            string query = "DELETE FROM login.tbllogin WHERE Username=@Username AND Password=@Password";
+            MySqlCommand sqlcmd = new MySqlCommand(query, conn);
+            sqlcmd.Parameters.AddWithValue("@Username", L.Username);
+            sqlcmd.Parameters.AddWithValue("@Password", L.Password);
+
+            sqlcmd.ExecuteScalar();
+
+            MessageBox.Show("GELUKT!");
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace prjloginmetlagenoefenenenenenen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNewPass = new System.Windows.Forms.TextBox();
+            this.lblNewPass = new System.Windows.Forms.Label();
+            this.btnWijzig = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtWachtwoord = new System.Windows.Forms.TextBox();
+            this.txtGebruikersnaam = new System.Windows.Forms.TextBox();
+            this.lblWachtwoord = new System.Windows.Forms.Label();
+            this.lblGebruikersnaam = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNewPass
+            // 
+            this.txtNewPass.Location = new System.Drawing.Point(117, 262);
+            this.txtNewPass.Name = "txtNewPass";
+            this.txtNewPass.Size = new System.Drawing.Size(100, 20);
+            this.txtNewPass.TabIndex = 34;
+            // 
+            // lblNewPass
+            // 
+            this.lblNewPass.AutoSize = true;
+            this.lblNewPass.Location = new System.Drawing.Point(12, 262);
+            this.lblNewPass.Name = "lblNewPass";
+            this.lblNewPass.Size = new System.Drawing.Size(98, 13);
+            this.lblNewPass.TabIndex = 33;
+            this.lblNewPass.Text = "Nieuw wachtwoord";
+            // 
+            // btnWijzig
+            // 
+            this.btnWijzig.Location = new System.Drawing.Point(117, 200);
+            this.btnWijzig.Name = "btnWijzig";
+            this.btnWijzig.Size = new System.Drawing.Size(100, 23);
+            this.btnWijzig.TabIndex = 32;
+            this.btnWijzig.Text = "Wijzigen";
+            this.btnWijzig.UseVisualStyleBackColor = true;
+            this.btnWijzig.Click += new System.EventHandler(this.btnWijzig_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(117, 171);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(100, 23);
+            this.btnDel.TabIndex = 31;
+            this.btnDel.Text = "Verwijderen";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(117, 120);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(100, 23);
+            this.btnRegister.TabIndex = 30;
+            this.btnRegister.Text = "Registreer";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(117, 91);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 23);
+            this.btnLogin.TabIndex = 29;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtWachtwoord
+            // 
+            this.txtWachtwoord.Location = new System.Drawing.Point(117, 41);
+            this.txtWachtwoord.Name = "txtWachtwoord";
+            this.txtWachtwoord.PasswordChar = '*';
+            this.txtWachtwoord.Size = new System.Drawing.Size(100, 20);
+            this.txtWachtwoord.TabIndex = 28;
+            // 
+            // txtGebruikersnaam
+            // 
+            this.txtGebruikersnaam.Location = new System.Drawing.Point(117, 9);
+            this.txtGebruikersnaam.Name = "txtGebruikersnaam";
+            this.txtGebruikersnaam.Size = new System.Drawing.Size(100, 20);
+            this.txtGebruikersnaam.TabIndex = 27;
+            // 
+            // lblWachtwoord
+            // 
+            this.lblWachtwoord.AutoSize = true;
+            this.lblWachtwoord.Location = new System.Drawing.Point(28, 41);
+            this.lblWachtwoord.Name = "lblWachtwoord";
+            this.lblWachtwoord.Size = new System.Drawing.Size(68, 13);
+            this.lblWachtwoord.TabIndex = 26;
+            this.lblWachtwoord.Text = "Wachtwoord";
+            // 
+            // lblGebruikersnaam
+            // 
+            this.lblGebruikersnaam.AutoSize = true;
+            this.lblGebruikersnaam.Location = new System.Drawing.Point(12, 9);
+            this.lblGebruikersnaam.Name = "lblGebruikersnaam";
+            this.lblGebruikersnaam.Size = new System.Drawing.Size(84, 13);
+            this.lblGebruikersnaam.TabIndex = 25;
+            this.lblGebruikersnaam.Text = "Gebruikersnaam";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 311);
+            this.Controls.Add(this.txtNewPass);
+            this.Controls.Add(this.lblNewPass);
+            this.Controls.Add(this.btnWijzig);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtWachtwoord);
+            this.Controls.Add(this.txtGebruikersnaam);
+            this.Controls.Add(this.lblWachtwoord);
+            this.Controls.Add(this.lblGebruikersnaam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNewPass;
+        private System.Windows.Forms.Label lblNewPass;
+        private System.Windows.Forms.Button btnWijzig;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtWachtwoord;
+        private System.Windows.Forms.TextBox txtGebruikersnaam;
+        private System.Windows.Forms.Label lblWachtwoord;
+        private System.Windows.Forms.Label lblGebruikersnaam;
+    }
+}
+
