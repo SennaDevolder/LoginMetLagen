d107cbe03cd7337e1fbd051928629768d2655736
senna.devolder <senna.devolder@sintjozefbrugge.be>
2024-11-26 13:49:34 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using prjloginmetlagenoefenenenenenen.DA;
+using prjloginmetlagenoefenenenenenen.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjloginmetlagenoefenenenenenen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Login L = new Login();
+            L.Username = txtGebruikersnaam.Text;
+            L.Password = txtWachtwoord.Text;
+
+            LoginDA.LoginValidation(L);
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            Login L = new Login();
+            L.Username = txtGebruikersnaam.Text;
+            L.Password = txtWachtwoord.Text;
+
+            LoginDA.AddUser(L);
+        }
+
+        private void btnWijzig_Click(object sender, EventArgs e)
+        {
+            Login L = new Login();
+            L.Username = txtGebruikersnaam.Text;
+            L.Password = txtWachtwoord.Text;
+
+            LoginDA.ChangePass(L, txtNewPass.Text);
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            Login L = new Login();
+            L.Username = txtGebruikersnaam.Text;
+            L.Password = txtWachtwoord.Text;
+
+            LoginDA.Delete(L);
+        }
+    }
+}
---
+++ Helper/Databank.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace prjloginmetlagenoefenenenenenen.Helper
+{
+    public class Databank
+    {
+        public static MySqlConnection MakeConnection()
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
+            {
+                Server = "Localhost",
+                Database = "login",
+                UserID = "root",
+                Password = "usbw",
+                ConnectionTimeout = 60,
+                Port = 3307,
+                AllowZeroDateTime = true
+            };
+
+            MySqlConnection conn = new MySqlConnection(builder.ConnectionString);
+
+            conn.Open();
+            return conn;
+        }
+
+        public static void CloseConnection(MySqlConnection conn)
+        {
+            if(conn != null)
+            {
+                conn.Close();
+                conn = null;
+            }
+        }
+    }
+}
